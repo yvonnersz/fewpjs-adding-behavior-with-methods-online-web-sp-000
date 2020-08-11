5ec96e1b34c49a8e110b03f4b01c3bbb324c8823
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-11 12:01:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ class Cat {
     return `${this.name} says meow!`
   }
 }
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    return `${dog.name} says woof!`
+  }
+}
