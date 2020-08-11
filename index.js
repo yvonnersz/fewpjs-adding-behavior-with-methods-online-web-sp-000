984f81857729e21dd3cbd33db07bfa299bf2aaf2
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-11 12:04:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ class Bird {
 
   speak() {
     if (this.sex === 'male') {
-    return `It's me! ${this.name}, the parrot!`
-  } else {
-    return `${this.name} says squawk!`
+      return `It's me! ${this.name}, the parrot!`
+    } else {
+      return `${this.name} says squawk!`
   }
 }
